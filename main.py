8f748e3b89e92d9bbe0c04f2ac0c85e2f5a4627b
Brandon Sov <9397850+brandonsov@users.noreply.github.com>
2021-07-27 11:50:38 -0700

    Add extra functionality to slack bot

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from typing import List
 
 load_dotenv()
 
+ABDUL_USER_ID = "U0291SC0HLN"
+
 # Install the Slack app and get xoxb- token in advance
 app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
@@ -26,10 +28,28 @@ def event_test(say):
 @app.event("message")
 def react_to_message(body, say, ack, client):
     ack()
+    print(f"{body}")
     channel_id = body["event"]["channel"]
     event_ts = body["event"]["event_ts"]
-    reactions = ["thumbsup", "thumbsdown"]
-    reaction_name = random.choice(reactions)
+    # reactions = ["thumbsup", "thumbsdown"]
+    reactions = client.emoji_list()["emoji"]
+    user_id = body["event"]["user"]
+    text = body["event"]["text"]
+    words = text.split()
+    for word in words:
+        if word in reactions.keys():
+            reactions.pop(word)
+            client.reactions_add(channel=channel_id, name=word, timestamp=event_ts)
+
+    try:
+        client.reactions_add(channel=channel_id, name=text, timestamp=event_ts)
+    except:
+        pass
+
+    if user_id == ABDUL_USER_ID:
+        reaction_name = "triggered_parrot"
+    else:
+        reaction_name = random.choice(list(reactions.keys()))
     client.reactions_add(channel=channel_id, name=reaction_name, timestamp=event_ts)
 
 
