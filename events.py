6e9d77c0d7e095c9683195a5344ea18a92b8d7e5
Brandon Sov <9397850+brandonsov@users.noreply.github.com>
2021-08-01 22:46:51 -0700

    Add logging

--- events.py
+++ events.py
@@ -1,6 +1,7 @@
 import random
-
 from app import app
+
+from logger import logging
 
 ABDUL_USER_ID = "U0291SC0HLN"
 
@@ -14,20 +15,23 @@ def event_test(say):
 def react_to_message(body, say, ack, client):
     ack()
 
-    print(f"{body}")  # Modify to logs
+    logging.info(f"{body}")
 
     channel_id = body["event"]["channel"]
     event_ts = body["event"]["event_ts"]
     user_id = body["event"]["user"]
-    reactions = {key.lower(): key for (key, _value) in client.emoji_list()["emoji"].items()}
+    reactions = {key.lower(): key for (key, _value)
+                 in client.emoji_list()["emoji"].items()}
     text = body["event"]["text"]
     for word in text.split():
         if word.lower() in reactions:
-            client.reactions_add(channel=channel_id, name=reactions[word.lower()], timestamp=event_ts)
+            client.reactions_add(
+                channel=channel_id, name=reactions[word.lower()], timestamp=event_ts)
             reactions.pop(word.lower())
 
     if user_id == ABDUL_USER_ID:
         reaction_name = "triggered_parrot"
     else:
         reaction_name = random.choice(list(reactions.values()))
-    client.reactions_add(channel=channel_id, name=reaction_name, timestamp=event_ts)
+    client.reactions_add(channel=channel_id,
+                         name=reaction_name, timestamp=event_ts)
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
-
-from dotenv import load_dotenv
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from dotenv import load_dotenv
 
 from app import app
+from logger import logging
 import events
 
 if __name__ == "__main__":
     load_dotenv()
+    logging.info("Started Slack Bot")
 
     handler = SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"])
     handler.start()
